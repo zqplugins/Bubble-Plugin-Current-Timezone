4db9d175bfacd40b1e1c6a30d745595b0a7ca122
zqplugins <137153217+zqplugins@users.noreply.github.com>
2024-02-14 02:32:07 +0200

    Automatic commit from Bubble

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -1,5 +1,53 @@
-function(instance, context) {
-  const opts = Intl.DateTimeFormat().resolvedOptions();
-  instance.publishState("locale", opts.locale);
-  instance.publishState("timezone", opts.timeZone);
+function(instance, context){
+	function _0x20ba(_0x4da396, _0x306d91) {
+    const _0x5a8ecf = _0x5a8e();
+    return _0x20ba = function (_0x20bab5, _0x410604) {
+        _0x20bab5 = _0x20bab5 - 0xcf;
+        let _0xaeb88f = _0x5a8ecf[_0x20bab5];
+        return _0xaeb88f;
+    }, _0x20ba(_0x4da396, _0x306d91);
+}
+function _0x5a8e() {
+    const _0x39eb1c = [
+        '9133ZBqGwY',
+        'DateTimeFormat',
+        '1977668XzkBES',
+        '332924vZlRdT',
+        '18QXQtDb',
+        'timezone',
+        'timeZone',
+        '6920530uMtBZm',
+        '6FIyUIC',
+        'resolvedOptions',
+        'publishState',
+        '2226512sAaWwe',
+        '2651376uAbNoX',
+        'locale',
+        '35gFnFHc',
+        '3932865MvzCuM'
+    ];
+    _0x5a8e = function () {
+        return _0x39eb1c;
+    };
+    return _0x5a8e();
+}
+(function (_0x3d3c73, _0x2041bd) {
+    const _0x4a2eae = _0x20ba, _0x27f26e = _0x3d3c73();
+    while (!![]) {
+        try {
+            const _0xf9f34c = parseInt(_0x4a2eae(0xd2)) / 0x1 * (parseInt(_0x4a2eae(0xda)) / 0x2) + -parseInt(_0x4a2eae(0xde)) / 0x3 + parseInt(_0x4a2eae(0xd5)) / 0x4 * (parseInt(_0x4a2eae(0xd0)) / 0x5) + -parseInt(_0x4a2eae(0xd6)) / 0x6 * (-parseInt(_0x4a2eae(0xd4)) / 0x7) + parseInt(_0x4a2eae(0xdd)) / 0x8 + parseInt(_0x4a2eae(0xd1)) / 0x9 + -parseInt(_0x4a2eae(0xd9)) / 0xa;
+            if (_0xf9f34c === _0x2041bd)
+                break;
+            else
+                _0x27f26e['push'](_0x27f26e['shift']());
+        } catch (_0x209e35) {
+            _0x27f26e['push'](_0x27f26e['shift']());
+        }
+    }
+}(_0x5a8e, 0x91c32));
+function initialize(_0x5db50c, _0x24e5e1) {
+    const _0x79c7b0 = _0x20ba, _0x140073 = Intl[_0x79c7b0(0xd3)]()[_0x79c7b0(0xdb)]();
+    _0x5db50c[_0x79c7b0(0xdc)]('locale', _0x140073[_0x79c7b0(0xcf)]), _0x5db50c['publishState'](_0x79c7b0(0xd7), _0x140073[_0x79c7b0(0xd8)]);
+}
+	 initialize(instance, context);
 }
